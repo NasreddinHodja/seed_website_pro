6b410a72104d4a6df3bb78d7440b6398a8a859d0
tbizetde@gmail.com <tbizetde@gmail.com>
2025-04-05 18:01:23 -0300

    nav bar content

--- components/Layouts/DefaultHeader.vue
+++ components/Layouts/DefaultHeader.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
-const items = HEADER_CHOICES;
+import header from "~/content/shared/header";
+
+const content = header["en"];
 </script>
 
 <template>
   <div
     class="w-full font-header flex justify-between items-end h-40 pb-2 px-16"
   >
-    <NuxtLink to="/"> <div class="text text-5xl w-60">S.E.E.D.</div></NuxtLink>
+    <NuxtLink to="/"
+      ><div class="text text-5xl w-60">{{ content["leftText"] }}</div></NuxtLink
+    >
     <div class="flex gap-2">
-      <div v-for="(item, index) in items" class="flex text-xl gap-2">
+      <div
+        v-for="(item, index) in content['navigation']"
+        class="flex text-xl gap-2"
+      >
         <div v-if="index > 0">｜</div>
         <NuxtLink :to="item.to">{{ item.label }}</NuxtLink>
       </div>
     </div>
     <div class="w-60 pb-1 flex justify-start">
       <NuxtLink to="/">
-        <img src="@/public/images/e_logo.jpg" class="w-36 min-w-36 ml-3" />
+        <img :src="content['rightImage']" class="w-36 min-w-36 ml-3" />
       </NuxtLink>
     </div>
   </div>
---
+++ content/shared/header.ts
@@ -0,0 +1,46 @@
+export default {
+  en: {
+    leftText: "S.E.E.D.",
+    navigation: [
+      {
+        label: "ABOUT",
+        to: "/about",
+      },
+      {
+        label: "WEB3",
+        to: "/web3",
+      },
+      {
+        label: "METHODS",
+        to: "/methods",
+      },
+      {
+        label: "MERCH",
+        to: "/merch",
+      },
+    ],
+    rightImage: "/images/e_logo.jpg"
+  },
+  pt: {
+    leftText: "S.E.E.D.",
+    navigation: [
+      {
+        label: "ABOUT",
+        to: "/about",
+      },
+      {
+        label: "WEB3",
+        to: "/web3",
+      },
+      {
+        label: "METHODS",
+        to: "/methods",
+      },
+      {
+        label: "MERCH",
+        to: "/merch",
+      },
+    ],
+    rightImage: "/images/e_logo.jpg"
+  }
+}
